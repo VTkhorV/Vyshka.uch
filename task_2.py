294ea36294563520d8ea584ef45ee6b0fff9a6e1
VTkhorV <thorophone@gmail.com>
2025-09-29 19:16:25 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,11 @@
+def greet(greet_text):
+    return greet_text
+
+print(greet(greet_text ='Hello, World!'))
+
+if __name__ == "__greet__":
+    # 2. Допишите строчку кода: 
+    # Здесь должно производиться присваивание переменной
+    # Мы должны занести в greet_text текст приветствия вместо None
+    greet_text = "Hello, world!"
+    greet(greet_text)
